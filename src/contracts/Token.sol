4f996469e2f47a3cd72bc1ae8641f9813aee25db
Shivesh Dindayal <shivy101@live.com>
2021-02-04 22:55:35 +0100

    changes

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,8 +1,26 @@
 // "SPDX-License-Identifier: UNLICENSED"
 pragma solidity ^0.5.16;
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-contract Token{
-    string public name = 'foo';
+contract Token {
+    string public name = "R2-D2";
+    string public symbol = "R2D";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
 
+    constructor() public {
+        totalSupply = 1000000 * (10**decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
 
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to]=balanceOf[_to].add(_value);
+        return true;
+
+    }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,13 +1,57 @@
+const { should } = require("chai");
+
 const Token = artifacts.require("./Token");
 
 require("chai").use(require("chai-as-promised")).should();
 
-contract("Token", (accounts) => {
+contract("Token", ([deployer]) => {
+  const name = "R2-D2";
+  const symbol = "R2D";
+  const decimals = "18";
+  const totalSupply = "1000000000000000000000000";
+  let token;
+  beforeEach(async () => {
+    token = await Token.new();
+  });
   describe("deployment", () => {
     it("tracks the name", async () => {
-      const token = await Token.new();
       const result = await token.name();
-      result.should.equal("foo");
+      result.should.equal(name);
+    });
+
+    it("tracks the symbol", async () => {
+      const result = await token.symbol();
+      result.should.equal(symbol);
+    });
+    it("tracks the decimals", async () => {
+      const result = await token.decimals();
+      result.toString().should.equal(decimals);
+    });
+    it("tracks the total supply", async () => {
+      const result = await token.totalSupply();
+      result.toString().should.equal(totalSupply);
+    });
+    it("assigns the total supply to the deployer", async () => {
+      const result = await token.balanceOf(deployer);
+      result.toString().should.equal(totalSupply);
+    });
+  });
+  describe("sending tokens", () => {
+    it("transfers token balances", async () => {
+      let balanceOf;
+      balanceOf = await token.balanceOf(deployer);
+      console.log("deployer balance before", balanceOf.toString());
+      balanceOf = await token.balanceOf(reciever);
+      console.log("reciever balance before transfer", balanceOf.toString());
+
+      await token.transfer(reciever, "100000000000000000000000", {
+        from: deployer,
+      });
+
+      balanceOf = await token.balanceOf(deployer);
+      console.log("deployer balance after", balanceOf.toString());
+      balanceOf = await token.balanceOf(reciever);
+      console.log("reciever balance after transfer", balanceOf.toString());
     });
   });
 });
