97c1ee007b279d5325beadcd31172f26b5289cb4
Shivesh Dindayal <shivy101@live.com>
2021-02-04 09:29:04 +0100

    init

---
+++ src/contracts/Token.sol
@@ -0,0 +1,8 @@
+// "SPDX-License-Identifier: UNLICENSED"
+pragma solidity ^0.5.16;
+
+contract Token{
+    string public name = 'foo';
+
+
+}
---
+++ test/Token.test.js
@@ -0,0 +1,13 @@
+const Token = artifacts.require("./Token");
+
+require("chai").use(require("chai-as-promised")).should();
+
+contract("Token", (accounts) => {
+  describe("deployment", () => {
+    it("tracks the name", async () => {
+      const token = await Token.new();
+      const result = await token.name();
+      result.should.equal("foo");
+    });
+  });
+});
