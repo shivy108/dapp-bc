69d6c274bdd7bd4fc064fa48017f6876f01e8bf8
Shivesh Dindayal <shivy101@live.com>
2021-02-15 15:08:44 +0100

    redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
-import Web3 from 'web3'
-import Token from '../abis/Token.json'
+import React, { Component } from "react";
+import "./App.css";
+import Web3 from "web3";
+import Token from "../abis/Token.json";
 
 class App extends Component {
   componentWillMount() {
     // this.loadBlockchainData()
-    this.getData()
+    this.getData();
   }
 
   getData = async () => {
@@ -15,29 +15,52 @@ class App extends Component {
     await window.ethereum.enable();
     const accounts = await web3.eth.getAccounts();
     console.log("TCL: getData -> accounts", accounts);
-    console.log("TCL: getData -> networkDataAddress", Token.networks[networkId].address);
-    const token = new web3.eth.Contract(Token.abi, Token.networks[networkId].address)
-    console.log('token', token)
+    console.log(
+      "TCL: getData -> networkDataAddress",
+      Token.networks[networkId].address
+    );
+    const token = new web3.eth.Contract(
+      Token.abi,
+      Token.networks[networkId].address
+    );
+    const totalSupply = await token.methods.totalSupply().call();
+    console.log("totalSupply", totalSupply);
   };
 
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <a className="navbar-brand" href="/#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <a className="navbar-brand" href="/#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link" href="/#">Link 1</a>
+                <a className="nav-link" href="/#">
+                  Link 1
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/#">Link 2</a>
+                <a className="nav-link" href="/#">
+                  Link 2
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/#">Link 3</a>
+                <a className="nav-link" href="/#">
+                  Link 3
+                </a>
               </li>
             </ul>
           </div>
@@ -45,63 +68,81 @@ class App extends Component {
         <div className="content">
           <div className="vertical-split">
             <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
+              <div className="card-header">Card Title</div>
               <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
               </div>
             </div>
             <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
+              <div className="card-header">Card Title</div>
               <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
               </div>
             </div>
           </div>
           <div className="vertical">
             <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
+              <div className="card-header">Card Title</div>
               <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
               </div>
             </div>
           </div>
           <div className="vertical-split">
             <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
+              <div className="card-header">Card Title</div>
               <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
               </div>
             </div>
             <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
+              <div className="card-header">Card Title</div>
               <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
               </div>
             </div>
           </div>
           <div className="vertical">
             <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
+              <div className="card-header">Card Title</div>
               <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
               </div>
             </div>
           </div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import configureStore from "./store/configureStore";
+import { Provider } from "react-redux";
 // import reportWebVitals from './reportWebVitals';
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
